7d211cc36b7247785de2ce8a1eed8f7e24846cd0
lifuyue <lif2@rose-hulman.edu>
2018-01-22 10:41:16 -0500

    m3 is finished

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  FOR and WHILE loops.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Li Fuyue.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -45,7 +45,7 @@ def main():
 def run_test_practice_problem3():
     """ Tests the   practice_problem3  function. """
     ####################################################################
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  practice_problem3  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -135,6 +135,16 @@ def run_test_practice_problem3():
     #             that they are adequate tests!
     ####################################################################
 
+    # 15th test:
+    st.SimpleTestCase(practice_problem3,
+                      [-2, 2, 0],
+                      [-5, -4])
+
+    # 16th test:
+    st.SimpleTestCase(practice_problem3,
+                      [-2, 2, 1],
+                      [-5, 1])
+
 
 def practice_problem3(start, n, threshold):
     """
@@ -208,7 +218,7 @@ def practice_problem3(start, n, threshold):
       :type threshold: float
     """
     ####################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above),
     #          but you are required to write ADDITIONAL tests (above).
     ####################################################################
@@ -216,6 +226,15 @@ def practice_problem3(start, n, threshold):
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   < 15 minutes.
     ####################################################################
+    new_list = []
+    count = 0
+    for i in range(999999):
+        if count < n:
+            if math.sin(start + i) + math.cos(start + i) > threshold:
+                new_list += [start + i]
+                count += 1
+
+    return new_list
 
 
 # ----------------------------------------------------------------------
